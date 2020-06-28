f5dde8f854b79648acb6a5e02a514d4e21989c97
Yehonatan Shaneh <yshaneh@gmail.com>
2020-06-28 08:29:55 +0300

    fixed bug

--- server.py
+++ server.py
@@ -212,14 +212,14 @@ def handle_command(message, conn):
             username = params[1 - index]
             exist, c = get_conn_by_username(username)
             if not exist:
-                if params[0][0] != "-":
-                    if not users.user_exist(params[0]):
-                        message = "user %s does not exsit!" % params[0]
+                if username[0] != "-":
+                    if not users.user_exist(username):
+                        message = "user %s does not exsit!" % username
                     else:
-                        message = "user %s is not connect to server" % params[0]
+                        message = "user %s is not connect to server" % username
                 send_message(conn, message)
                 return True
-            send_message(conn, "[%s] %s : %s" % (users.users[username]['type'], params[0], client_ip[c]))
+            send_message(conn, "[%s] %s : %s" % (users.users[username]['type'], username, client_ip[c]))
         else:
             send_message(conn, "invaild useage! type '!help users'")
     return True
